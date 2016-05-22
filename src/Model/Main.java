3366eafd3859e50dc8391dec171fb21e0c8090d8
Toan <nmtoan07@gmail.com>
2016-05-23 00:06:38 +0700

    Add base source

---
+++ src/Model/Main.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Environment environment = Environment.getInstance();
+        environment.createEnvironment(20, 20);
+        environment.printMatrix();
+        Robot robot = Robot.getIntance();
+
+        robot.update(environment, 8, 12);
+        robot.start();
+        System.out.println("Matrix final!");
+        environment.printMatrix();
+    }
+
+}
